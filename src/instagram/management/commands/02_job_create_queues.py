1f77a54a7ef08774027b3e0e3f734f2b874b5efc
Fernando Espíndola <fer.esp@gmail.com>
2018-12-10 01:10:35 -0200

    update

--- src/instagram/management/commands/02_job_create_queues.py
+++ src/instagram/management/commands/02_job_create_queues.py
@@ -1,13 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from fe_azure.queue import create_queues, QUEUE_TEXT_SEARCH
+from fe_azure.queue import create_queues, QUEUE_TEXT_SEARCH, QUEUE_HASHTAG
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         create_queues(QUEUE_TEXT_SEARCH)
-        # create_queues(QUEUE_JOB_EXTRACT_HASHTAGS_FROM_TEXT_SEARCH)
-        # create_queues(QUEUE_JOB_EXTRACT_HASHTAG_COUNT)
-        # create_queues(QUEUE_JOB_EXTRACT_POST_FROM_HASHTAG)
-        # create_queues(QUEUE_JOB_PREPARE_POST_TO_IMPORT_TO_ELASTICSEARCH)
+        create_queues(QUEUE_HASHTAG)
--- src/instagram/management/commands/03_job_extract_hashtags_from_text_search.py
+++ src/instagram/management/commands/03_job_extract_hashtags_from_text_search.py
@@ -15,19 +15,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         while True:
+            time.sleep(1)
             django.db.close_old_connections()
             message = receive_queue_message(QUEUE_TEXT_SEARCH)
-            if message:
+            if message is None:
+                logger.info(f'The message is {message}')
                 continue
             if message.body is None:
-                logger.info('The message body is None')
+                logger.info(f'The message body is {message.body}')
             else:
+                logger.info(f'The message body is {message.body}')
                 text_search_uuid = message.body.decode("utf-8")
                 logger.info(f'text_search_uuid: {text_search_uuid}')
                 text_search = TextSearch.objects.get(uuid=text_search_uuid)
                 hashtags = text_search.get_hashtags_from_result()
                 for hashtag in hashtags:
-                    logger.info(f'Processing tag: {hashtag}')
+                    logger.info(f'Processing hashtag: {hashtag}')
                     send_to_queue_hashtag(hashtag)
                     Tag.objects.get_or_create(name=hashtag)
 
@@ -38,4 +41,3 @@ class Command(BaseCommand):
             except AzureServiceBusPeekLockError:
                 logger.info('Timeout getting new message!')
             logger.info('Sleep for 1 seconds...')
-            time.sleep(1)
